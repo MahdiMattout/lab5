df4115859ed3092086abf1500f69f86a35a929a7
Mahdi Mattout <mahdim@lemonadefashion.com>
2022-03-04 23:35:12 +0200

    done

--- form.js
+++ form.js
@@ -1,5 +1,8 @@
 const $ = (id) => { return document.getElementById(id); }
 const slider = $('slider');
+$('addressDetailsForm').style.display='none';
+$('orderSummaryForm').style.display='none';
+document.body.style.backgroundColor = '#01dddd';
 
 
 const getSize = () => {
@@ -10,7 +13,7 @@ const checkedToppings = (id) => {
   var checked = [];
   var inputTags = $(id).getElementsByTagName('input');
   for (let i = 0; i < inputTags.length; i++){
-    if (inputTags[i].checked) { checked.push(inputTags[i].value); }
+    if (inputTags[i].checked) { checked.push(inputTags[i]); }
   }
   return checked;
 }
@@ -41,6 +44,7 @@ const _getImageSize = (sliderValue) => {
       return '200px';
     case '4':
       return '250px';
+    default: '100px'
   }
 }
 
@@ -54,19 +58,23 @@ const _getPizzaSize = (sliderValue) => {
       return 'Large';
     case '4':
       return 'X-Large';
+    default:
+      return 'Small';
   }
 }
 
 const _getPizzaPrice = (sliderValue) => {
   switch (sliderValue) {
     case '1':
-      return '6';
+      return 6;
     case '2':
-      return '10';
+      return 10;
     case '3':
-      return '14';
+      return 14;
     case '4':
-      return '16';
+      return 16;
+    default:
+      return 6;
   }
 }
 
@@ -91,11 +99,99 @@ const _getVegToppingsPrice = () => {
 }
 
 const getTotal = (sliderValue) => {
-  return _getPizzaPrice(sliderValue) + _getMeatToppingPrice() + _getVegToppingsPrice() + (getCheese == '3' ? 3 : 0);
+  return _getPizzaPrice(sliderValue) + _getMeatToppingPrice() + _getVegToppingsPrice() + (getCheese() == 3 ? 3 : 0);
 }
 
 
 const fillSummary = () => {
+  const orderSummaryP = $('dlvrTo');
+  const firstNameField = $('firstNameField').value;
+  const lastNameField = $('lastNameField').value;
+  const emailField = $('emailField').value;
+  const phoneNumberField = $('phoneNumberField').value;
+  const citySelect = $('citySelect').value;
+  const addressTextArea = $('addressTextArea').value;
+  orderSummaryP.textContent =
+    `${firstNameField} ${lastNameField}, ${emailField}, ${phoneNumberField}, ${citySelect} - ${addressTextArea}`;
   
+  const orderList = $('orderList');
+  orderList.innerHTML = ""
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(`${_getPizzaSize(slider.value)} size`))
+  orderList.append(li);
+  const checkedArr = checkedToppings('form1');
+  for(let i = 0; i < checkedArr.length; i++) {
+    if (checkedArr[i].getAttribute('name') != 'paymentRadio') {
+      li = document.createElement("li");
+      li.appendChild(document.createTextNode(checkedArr[i].value));
+      orderList.appendChild(li);
+    }
+  }
+  const total = $('total');
+  total.innerHTML = `Total: ${getTotal()}$`;
 }
+
+const goToPage = (pageNumber) => {
+  switch (pageNumber) {
+    case 1:
+      $('form1').style.display = 'inherit';
+      $('addressDetailsForm').style.display = 'none';
+      $('orderSummaryForm').style.display = 'none';
+      document.body.style.backgroundColor = '#01dddd';
+      break;
+    case 2:
+      $('form1').style.display = 'none';
+      $('addressDetailsForm').style.display = 'inherit';
+      $('orderSummaryForm').style.display = 'none';
+      document.body.style.backgroundColor = '#e93a57';
+      break;
+    case 3:
+      $('form1').style.display = 'none';
+      $('addressDetailsForm').style.display = 'none';
+      $('orderSummaryForm').style.display = 'inherit';
+      document.body.style.backgroundColor = '#3fc38e';
+      break;
+    default:
+      goToPage(1);
+  }
+}
+
+const checkInfo = () => {
+  const addressDetailsForm = $('addressDetailsForm');
+  const fields = addressDetailsForm.getElementsByClassName('addressDetailsFormFields');
+  for(let i = 0; i < fields.length; i++) {
+    if (fields[i].value === '') return false;
+  }
+  return true;
+}
+
+const form1Button = $('form1NextButton');
+form1Button.addEventListener('click', (e) => {
+  e.preventDefault();
+  goToPage(2);
+})
+
+const addressDetailsFormBackButton = $('addressDetailsFormBackButton')
+addressDetailsFormBackButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  goToPage(1);
+})
+
+const addressDetailsFormNextButton = $('addressDetailsFormNextButton')
+addressDetailsFormNextButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (!checkInfo()) {
+    alert('All forms must be filled!');
+    return;
+  }
+  goToPage(3);
+  fillSummary();
+})
+
+const orderSummaryBackButton = $('orderSummaryBackButton')
+orderSummaryBackButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  goToPage(2);
+})
+ 
 
